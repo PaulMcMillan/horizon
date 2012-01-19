4c260fbb60ae2fb314f29e2ecc3dcb9055171236
Paul McMillan <paul.mcmillan@nebula.com>
2012-01-18 21:53:57 -0800

    more changes
    Change-Id: I1e955834500ed1e74aafe2516a7ddffd93aebf16

--- horizon/horizon/dashboards/nova/networks/urls.py
+++ horizon/horizon/dashboards/nova/networks/urls.py
@@ -20,13 +20,13 @@
 
 from django.conf.urls.defaults import patterns, url
 
-from .views import IndexView, CreateView
+from .views import IndexView, CreateView, RenameView
 
 urlpatterns = patterns('horizon.dashboards.nova.networks.views',
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^create/$', CreateView.as_view(), name='create'),
     url(r'^(?P<network_id>[^/]+)/detail/$', 'detail', name='detail'),
-    url(r'^(?P<network_id>[^/]+)/rename/$', 'rename', name='rename'),
+    url(r'^(?P<network_id>[^/]+)/rename/$', RenameView.as_view(), name='rename'),
     url(r'^(?P<network_id>[^/]+)/ports/create/$', 'port_create',
         name='port_create'),
     url(r'^(?P<network_id>[^/]+)/ports/(?P<port_id>[^/]+)/attach/$',
